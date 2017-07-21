8b1f0fc9e24caf71d1d6881a3e9444287f5228bb
Michael.Canova <Michael.Canova@schwab.com>
2017-07-21 16:03:08 -0500

    first commit

---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace SuperAdventure
+{
+    using System.Media;
+
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            this._player = new Player()
+                               {
+                                   CurrentHitPoints = 10,
+                                   MaximumHitPoints = 10,
+                                   Gold = 20,
+                                   ExperiencePoints = 0,
+                                   Level = 1
+                               };
+
+            this.lblHitPoints.Text = this._player.CurrentHitPoints.ToString();
+            this.lblGold.Text = this._player.Gold.ToString();
+            this.lblExperience.Text = this._player.ExperiencePoints.ToString();
+            this.lblLevel.Text = this._player.Level.ToString();
+        }
+
+        private void SuperAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
